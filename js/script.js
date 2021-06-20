07596a2ff35fc1edaf9ab9a916bbb11315a7f579
ThunderR97 <arthur3730@hotmail.no>
2021-06-20 23:31:46 +0200

    Updated

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//Question 1
+
+const cat =  {
+    complain: true,
+}
+
+if(cat.complain) {
+    console.log("Meow!");
+}
+
+//Question 2
+
+const h3style = {
+    title: "Updating heading",
+};
+
+//const h3style = document.querySelector(".h3");
+
+//if (!h3style) {
+//
+//}
+
+//h3style.innerHTML = "
+//<h3> type=$(h3style.title)<h3>a
+//"
+
+//Question 3
